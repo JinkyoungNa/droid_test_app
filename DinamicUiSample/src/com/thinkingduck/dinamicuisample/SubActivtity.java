c4c46c08adff50c7b16c647677ddbc76755e57bc
jinkyoung na <kamjike@gmail.com>
2012-12-13 12:35:31 +0900

    add extend Activity to subActivity

---
+++ DinamicUiSample/src/com/thinkingduck/dinamicuisample/SubActivtity.java
@@ -0,0 +1,13 @@
+package com.thinkingduck.dinamicuisample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubActivtity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+}
